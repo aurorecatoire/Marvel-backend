3175eefdf5c85c3200ae34b28ecd892ba98c7adc
aurore catoire <aurore.catoire@gmail.com>
2025-11-26 15:20:17 +0100

    les liens fonctionnent

--- index.js
+++ index.js
@@ -7,13 +7,72 @@ const API_KEY = process.env.API_KEY;
 const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.json({ message: "server on" });
+  res.json({ message: "server on !" });
 });
+
+//COMICS
+//Liste des comics 
+
+app.get("/comics", async (req, res) => {
+  try {
+    const result = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${API_KEY}`
+    );
+    res.json(result.data);
+  } catch (error) {
+    res.status(500).json({ error: "Erreur API" });
+  }
+});
+
+//COMICS/CHARACTER_ID
+//Toutes les informations sur un personnage dans un comic spécifique 
+
+app.get("/comics/:characterId", async (req, res) => {
+  try {
+    const result = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics/5fc8ba1fdc33470f788f88b3?apiKey=${API_KEY}`
+    );
+    res.json(result.data);
+  } catch (error) {
+    res.status(500).json({ error: "Erreur API" });
+  }
+});
+
+//COMIC_ID
+//Toutes les informations sur un comic spécifique 
+
+app.get("/comic/:comicId", async (req, res) => {
+  try {
+    const result = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comic/5fce13de78edeb0017c92d68?apiKey=${API_KEY}`
+    );
+    res.json(result.data);
+  } catch (error) {
+    res.status(500).json({ error: "Erreur API" });
+  }
+});
+
+//CHARACTERS
+//Liste des personnages 
 
 app.get("/characters", async (req, res) => {
   try {
     const result = await axios.get(
-      `https://lereacteur-marvel-api.netlify.app/characters?apiKey=${API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${API_KEY}`
+    );
+    res.json(result.data);
+  } catch (error) {
+    res.status(500).json({ error: "Erreur API" });
+  }
+});
+
+//CHARACTER_ID
+//Récupérer les informations sur un personnage en particulier 
+
+app.get("/character/:characterId", async (req, res) => {
+  try {
+    const result = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/character/5fcf91f4d8a2480017b91453?apiKey=${API_KEY}`
     );
     res.json(result.data);
   } catch (error) {
